615b7b0b72abdfafc5b6e72c165f63277b3755cb
Kwann <nicha14c@gmail.com>
2026-01-09 22:19:20 +0700

    Move login routes above wildcard

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
 
+  # 1. หน้าแรกและ Route ปกติ
   root "home#index"
   get "home/index"
 
+  # 2. *** เอา Login/Logout มาแทรกไว้ตรงนี้ (ก่อนบรรทัด *path) ***
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
+  # 3. Resources อื่นๆ
   resources :users, only: [:index, :show, :new, :create, :edit, :update]
   resources :colors, only: [:index, :show, :new, :create, :edit, :update]
 
@@ -15,10 +22,7 @@ Rails.application.routes.draw do
   get "about" => "pages#about"
   get "contact" => "pages#contact"
 
+  # 4. *** ตัวดัก Error 404 ต้องอยู่ "ล่างสุดของไฟล์" เสมอครับ ***
   match "*path", to: "application#not_found", via: :all
-
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  get '/logout', to: 'sessions#destroy'
 
 end
